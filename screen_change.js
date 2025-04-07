20362fe5beded9b3e80c87796f2936d17c0f4b6a
ThePomoli <biggigi1314@gmail.com>
2025-04-08 01:34:01 +0800

    Update screen_change.js

--- screen_change.js
+++ screen_change.js
@@ -238,8 +238,8 @@ $(document).ready(function () {
                 afterVideo.play().catch(console.warn)
 
                 afterVideo.addEventListener('ended', function () {
-                    $('#afterplaypage').fadeOut(200, function () {
-                        $('#loadingpage').fadeIn(1000, function () {
+                    $('#afterplaypage').fadeOut(0, function () {
+                        $('#loadingpage').fadeIn(800, function () {
                             setTimeout(() => {
                                 location.reload();
                             }, 10000); // 10秒 = 10000ms
