e1c8207db98544e7e5b131fc09696072d160c142
ThePomoli <biggigi1314@gmail.com>
2025-04-26 14:02:51 +0800

    Update main_fish.js

--- main_fish.js
+++ main_fish.js
@@ -393,8 +393,8 @@ let myP5 = new p5((p) => {
 
 
             if (this.landed && this.landingTarget) {
-                this.position.x = p.lerp(this.position.x, this.landingTarget.x, 0.05);
-                this.position.y = p.lerp(this.position.y, this.landingTarget.y, 0.05);
+                this.position.x = p.lerp(this.position.x, this.landingTarget.x, 0.03);
+                this.position.y = p.lerp(this.position.y, this.landingTarget.y, 0.03);
                 return;
             }
 
